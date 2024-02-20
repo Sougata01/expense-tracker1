e4a65e9ac7aeac76b08af096449d132767d4fc09
Sougata01 <sougatadas01011999@gmail.com>
2024-02-20 21:15:13 +0530

    getElementsByClassName Method

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 // Write your code below:
-const mainHeading = document.getElementById('main-heading')
-mainHeading.textContent='Fruit World'
-mainHeading.style.color='orange'
-const Header = document.getElementById('header')
-Header.style.backgroundColor='green'
-Header.style.borderBottom='3px solid orange'
-const basketHeading = document.getElementById('basket-heading')
-basketHeading.style.color='green'
-const thanks = document.getElementById('thanks')
-thanks.innerHTML='<p>Please visit us again</p>'
+const fruit = document.getElementsByClassName('fruit')
+fruit[2].style.backgroundColor='yellow'
+for(let i=0; i<fruit.length; i++){
+  fruit[i].style.fontWeight='bold'
+}
