be120c4ef5a8863602b56e2a584b01ffc6dfee70
Sougata01 <sougatadas01011999@gmail.com>
2024-02-23 13:36:05 +0530

    add and delete

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
-// Write your code below:
-
-// creating subHeading element
-const subHeading = document.createElement('h3')
-const headingtext = document.createTextNode('Buy high organic fruits online')
-subHeading.appendChild(headingtext)
-
-// getting div
-const div = document.getElementsByTagName('div')
-
-// inserting element in first div
-const firstdiv = div[0]
-firstdiv.appendChild(subHeading)
-
-// Make the sub-heading text italic
-subHeading.style.fontStyle = 'italic'
-
-// creating paragraph element
-const para = document.createElement('p')
-const paratext = document.createTextNode('Total fruits: 4')
-para.appendChild(paratext)
-
-// getting ul
+const form = document.querySelector('form')
 const fruits = document.querySelector('.fruits')
-
-// inserting in second div
-const seconddiv = div[1]
-seconddiv.insertBefore(para,fruits)
-
-// setting id
-para.id = 'fruits-total'
+// adding
+form.addEventListener('submit',function(event){
+    event.preventDefault()
+    // selected the input element
+    const fruitToAdd = document.getElementById('fruit-to-add')
+    // created the li
+    const newLi = document.createElement('li')
+    newLi.innerHTML = fruitToAdd.value + '<button class="delete-btn">x</button>'
+    newLi.className = 'fruit'
+    // adding li as the last element of unordered list
+    fruits.appendChild(newLi)
+})
+// deleting
+fruits.addEventListener('click',function(event){
+    if(event.target.classList.contains('delete-btn')){
+        const fruitsToDelete = event.target.parentElement
+        fruits.removeChild(fruitsToDelete)
+    }
+})
