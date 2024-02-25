be26d1f018079c8cfbf82e534606b4b34343f53e
Sougata01 <sougatadas01011999@gmail.com>
2024-02-25 19:45:11 +0530

    save to local storage added

--- index.js
+++ index.js
@@ -1,43 +1,24 @@
-// Add input element inside form, before button, to take fruit description
-const inputElement = document.createElement('input')
-inputElement.id = 'description'
-const form = document.querySelector('form');
-const formButton = form.lastElementChild
-form.insertBefore(inputElement,formButton)
+// Write your code below
+// grabbing form as event is to be added there
+// eventlistener
+// const form = document.querySelector('form')
+// form.addEventListener('submit',function(event){
+//     const username = event.target.username.value
+//     const email = event.target.email.value
+//     const phone = event.target.phone.value
+//     localStorage.setItem("name",username)
+//     localStorage.setItem("email",email)
+//     localStorage.setItem("phone",phone)
+// })
 
-// Show the fruit description in italics on the next line
-const fruitItems = document.getElementsByClassName('fruit');
-const fruitDesc = [
-    'Banana is a tropical fruit',
-    'Apple is too costly',
-    'Orange is one of the most popular fruits',
-    'King of fruits',
-];
-for (let i = 0; i < fruitItems.length; i++) {
-    const para = document.createElement('p');
-    para.setAttribute('style', 'font-style:italic');
-    para.setAttribute('class', 'fruit-desc');
-    const textFor_para = document.createTextNode(fruitDesc[i]);
-    para.appendChild(textFor_para);
 
-    const placefor_para = fruitItems[i].lastElementChild;
-    fruitItems[i].insertBefore(para, placefor_para);
+// eventhandler
+function handleFormSubmit(event){
+    event.preventDefault()
+    const storedUsername = event.target.username.value
+    const storedEmail = event.target.email.value
+    const storedPhone = event.target.phone.value
+    localStorage.setItem("Username",storedUsername)
+    localStorage.setItem("Email",storedEmail)
+    localStorage.setItem("Phone",storedPhone)
 }
-
-// Create a filter that shows only those fruits whose either name or description or both matches the entered text
-const filter = document.getElementById('filter')
-filter.addEventListener("keyup", function (event) {
-    const textEntered = event.target.value.toLowerCase()
-    const fruitItems = document.getElementsByClassName('fruit')
-    for (let i = 0; i < fruitItems.length; i++) {
-        const currentFruitText = fruitItems[i].firstChild.textContent.toLowerCase()
-        const currentFruitDescription = fruitItems[i].firstElementChild.textContent.toLowerCase()
-        const currentFruit = currentFruitText + currentFruitDescription
-        if (currentFruit.indexOf(textEntered) === -1) {
-            fruitItems[i].style.display = "none"
-        }
-        else {
-            fruitItems[i].style.display = 'flex'
-        }
-    }
-})
