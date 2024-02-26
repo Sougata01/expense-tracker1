5fd992e6a406e52c8e6488299580fd19a32c2fc9
Sougata01 <sougatadas01011999@gmail.com>
2024-02-26 12:14:18 +0530

    local Storage save as object

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-// Write your code below
-// grabbing form as event is to be added there
-// eventlistener
-// const form = document.querySelector('form')
-// form.addEventListener('submit',function(event){
-//     const username = event.target.username.value
-//     const email = event.target.email.value
-//     const phone = event.target.phone.value
-//     localStorage.setItem("name",username)
-//     localStorage.setItem("email",email)
-//     localStorage.setItem("phone",phone)
-// })
-
-
-// eventhandler
 function handleFormSubmit(event){
     event.preventDefault()
-    const storedUsername = event.target.username.value
-    const storedEmail = event.target.email.value
-    const storedPhone = event.target.phone.value
-    localStorage.setItem("Username",storedUsername)
-    localStorage.setItem("Email",storedEmail)
-    localStorage.setItem("Phone",storedPhone)
+    // inititalize input texts
+    const username = event.target.username.value
+    const email = event.target.email.value
+    const phone = event.target.phone.value
+    // object created
+    const userobj = {
+        Username: username,
+        Email: email,
+        Phone: phone
+    }
+    // sterialized
+    const userobj_serialized = JSON.stringify(userobj)
+    // storing to localStorage
+    localStorage.setItem("User Details",userobj_serialized)
+    console.log(event)
 }
