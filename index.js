68456f045941ab60db948f156e35ad7e473a0439
Sougata01 <sougatadas01011999@gmail.com>
2024-02-26 17:54:26 +0530

    how to scale

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-function handleFormSubmit(event){
+// Write your code below:
+// storing to local storage
+function handleFormSubmit(event) {
     event.preventDefault()
     // inititalize input texts
-    const username = event.target.username.value
-    const email = event.target.email.value
-    const phone = event.target.phone.value
+    const user_name = event.target.username.value
+    const e_mail = event.target.email.value
+    const phone_no = event.target.phone.value
     // object created
     const userobj = {
-        Username: username,
-        Email: email,
-        Phone: phone
+        username: user_name,
+        email: e_mail,
+        phone: phone_no
     }
     // sterialized
     const userobj_serialized = JSON.stringify(userobj)
     // storing to localStorage
-    localStorage.setItem("User Details",userobj_serialized)
-    console.log(event)
+    localStorage.setItem(e_mail, userobj_serialized)
+
+    // adding unordered list
+    const ul = document.querySelector('ul')
+    const Li = document.createElement('li')
+    const LiText = document.createTextNode(user_name + ' - ' + e_mail + ' - ' + phone_no)
+    Li.appendChild(LiText)
+    ul.appendChild(Li)
 }
+module.exports = handleFormSubmit
