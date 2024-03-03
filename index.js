6041a989f0d3b8fa8b7d6d494d031c0ed6fac63d
Sougata01 <sougatadas01011999@gmail.com>
2024-03-03 13:10:53 +0530

    edit button added

--- index.js
+++ index.js
@@ -3,25 +3,49 @@
 function handleFormSubmit(event) {
     event.preventDefault()
     // inititalize input texts
-    const user_name = event.target.username.value
-    const e_mail = event.target.email.value
-    const phone_no = event.target.phone.value
+    const username = event.target.username.value
+    const email = event.target.email.value
+    const phone = event.target.phone.value
     // object created
     const userobj = {
-        username: user_name,
-        email: e_mail,
-        phone: phone_no
+        username,
+        email,
+        phone
     }
     // sterialized
     const userobj_serialized = JSON.stringify(userobj)
     // storing to localStorage
-    localStorage.setItem(e_mail, userobj_serialized)
-
-    // adding unordered list
-    const ul = document.querySelector('ul')
-    const Li = document.createElement('li')
-    const LiText = document.createTextNode(user_name + ' - ' + e_mail + ' - ' + phone_no)
-    Li.appendChild(LiText)
-    ul.appendChild(Li)
+    localStorage.setItem(userobj.email, userobj_serialized)
+    showUserOnScreen(userobj)
 }
+
+function showUserOnScreen(obj) {
+    // adding unordered list
+    const parentElem = document.getElementById('listofItems')
+    const childElem = document.createElement('li')
+    childElem.textContent = obj.username + ' - ' + obj.email + ' - ' + obj.phone
+    const deleteButton = document.createElement('button')
+    deleteButton.type = 'submit'
+    deleteButton.textContent = 'Delete'
+    deleteButton.onclick = () =>{
+        localStorage.removeItem(obj.email)
+        parentElem.removeChild(childElem)
+    }
+    const editButton = document.createElement('button')
+    editButton.type = 'submit'
+    editButton.textContent = 'Edit'
+    editButton.onclick = () =>{
+        localStorage.removeItem(obj.email)
+        parentElem.removeChild(childElem)
+        document.getElementById('username').value=obj.username
+        document.getElementById('email').value=obj.email
+        document.getElementById('phone').value=obj.phone
+    }
+
+
+    childElem.appendChild(editButton)
+    childElem.appendChild(deleteButton)
+    parentElem.appendChild(childElem)
+}
+
 module.exports = handleFormSubmit
